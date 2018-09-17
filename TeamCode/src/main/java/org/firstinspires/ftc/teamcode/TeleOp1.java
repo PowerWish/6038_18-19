3ab78b1c70e039ace9f82563bd6898417a5182bf
rohan <powerwish10@yahoo.com>
2018-09-17 15:49:19 -0700

    basic teleop complete

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp1.java
@@ -101,7 +101,8 @@ public class TeleOp1 extends OpMode
         // Setup a variable for each drive wheel to save power level for telemetry
         double leftPower;
         double rightPower;
-
+        leftDrive.setPower(gamepad1.left_stick_y);
+        rightDrive.setPower(gamepad1.right_stick_y);
         // Choose to drive using either Tank Mode, or POV Mode
         // Comment out the method that's not used.  The default below is POV.
 
